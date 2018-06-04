b96c81acc9bad6e5823a0cae4361ea8d779af3ca
kemairichardson <kemai_richardson@davincischools.org>
2018-06-04 16:35:44 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,117 @@
+function playsong1(e){
+	<!--This code allows you to click on an image and the image will play a song-->
+	document.getElementById("chris").play();
+	document.getElementById("anti").pause();
+	document.getElementById("feelins").pause();
+	document.getElementById("motor").pause();	
+	document.getElementById("nolimit").pause();
+	document.getElementById("america").pause();
+	document.getElementById("backup").pause();	
+	document.getElementById("needme").pause();
+	document.getElementById("bardi").pause();
+	document.getElementById("loveg").pause();
+
+}
+function playsong2(e){
+	<!--This code allows you to click on an image and the image will play a song-->
+	document.getElementById("anti").play();
+	document.getElementById("chris").pause();
+	document.getElementById("feelins").pause();
+	document.getElementById("motor").pause();	
+	document.getElementById("nolimit").pause();
+	document.getElementById("america").pause();
+	document.getElementById("backup").pause();
+	document.getElementById("needme").pause();	
+	document.getElementById("bardi").pause();
+	document.getElementById("loveg").pause();
+
+}
+
+function playsong3(e){
+	<!--This code allows you to click on an image and the image will play a song-->
+	document.getElementById("feelins").play();
+	document.getElementById("chris").pause();
+	document.getElementById("black").pause();
+	document.getElementById("nolimit").pause();
+	document.getElementById("america").pause();
+	document.getElementById("backup").pause();		
+	document.getElementById("needme").pause();
+	document.getElementById("bardi").pause();
+	document.getElementById("loveg").pause();
+}
+function playsong4(e){
+	<!--This code allows you to click on an image and the image will play a song-->
+	document.getElementById("motor").play();
+	document.getElementById("chris").pause();
+	document.getElementById("black").pause();
+	document.getElementById("feelins").pause();
+}
+function playsong5(e){
+	<!--This code allows you to click on an image and the image will play a song-->
+	document.getElementById("nolimit").play();
+	document.getElementById("chris").pause();
+	document.getElementById("black").pause();
+	document.getElementById("feelins").pause();
+	document.getElementById("motor").pause();
+
+}
+function playsong6(e){
+	<!--This code allows you to click on an image and the image will play a song-->
+	document.getElementById("america").play();
+	document.getElementById("chris").pause();
+	document.getElementById("black").pause();
+	document.getElementById("feelins").pause();
+	document.getElementById("motor").pause();
+	document.getElementById("nolimit").pause();
+
+}
+function playsong7(e){
+	<!--This code allows you to click on an image and the image will play a song-->
+	document.getElementById("backup").play();
+	document.getElementById("chris").pause();
+	document.getElementById("black").pause();
+	document.getElementById("feelins").pause();
+	document.getElementById("motor").pause();
+	document.getElementById("nolimit").pause();
+
+}
+function playsong8(e){
+	<!--This code allows you to click on an image and the image will play a song-->
+	document.getElementById("needme").play();
+	document.getElementById("chris").pause();
+	document.getElementById("black").pause();
+	document.getElementById("feelins").pause();
+	document.getElementById("motor").pause();
+	document.getElementById("nolimit").pause(); 
+	document.getElementById("america").pause();
+	document.getElementById("bardi").pause();
+	
+}
+function playsong9(e){
+	<!--This code allows you to click on an image and the image will play a song-->
+	document.getElementById("bardi").play();
+	document.getElementById("chris").pause();
+	document.getElementById("black").pause();
+	document.getElementById("feelins").pause();
+	document.getElementById("motor").pause();
+	document.getElementById("nolimit").pause();
+	document.getElementById("america").pause();
+	document.getElementById("backup").pause();		
+	document.getElementById("needme").pause();
+	document.getElementById("loveg").pause();
+
+}
+function playsong10(e){
+	<!--This code allows you to click on an image and the image will play a song-->
+	document.getElementById("loveg").play();
+	document.getElementById("chris").pause();
+	document.getElementById("black").pause();
+	document.getElementById("feelins").pause();
+	document.getElementById("motor").pause();
+	document.getElementById("nolimit").pause();
+	document.getElementById("america").pause();
+	document.getElementById("backup").pause();	
+	document.getElementById("needme").pause();
+	document.getElementById("bardi").pause();
+
+}
